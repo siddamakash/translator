21566f601abf833335f028b3b0d764405ebd17bd
siddamakash <80254994+siddamakash@users.noreply.github.com>
2021-04-23 19:17:59 +0530

    updated html doc

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ var outputDiv = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/pirate.json"
 
-function getTranslationURL(Text){
+function getTranslationURL(Text) {
 
-return severURL + "?" +"text="+ Text
+    return severURL + "?" + "text=" + Text
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server!try again after sometime")
 }
 
-function clickHandler(){
+function clickHandler() {
     var inputText = textInput.value;
 
-fetch(getTranslationURL(inputText))
-.then( Response => Response.json()) 
-.then(json => console.log(json.contents.translated))
-.catch(errorHandler)
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json =>{
+             var translatedText = json.contents.translated;
+             outputDiv.innerText = translatedText;
+            })
+
+        .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
 
 
 
