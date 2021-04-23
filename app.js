122fe513415ab1e1d03e11a77c818461adff3c1e
siddamakash <80254994+siddamakash@users.noreply.github.com>
2021-04-23 20:21:39 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var btnTranslate = document.querySelector("#btn-transalte");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
@@ -11,11 +11,11 @@ function getTranslationURL(Text) {
 
 function errorHandler(error) {
     console.log("error occured", error);
-    alert("something wrong with server!try again after sometime")
+    alert("something wrong with server!try again after sometime");
 }
 
 function clickHandler() {
-    var inputText = textInput.value;
+    var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
@@ -25,10 +25,9 @@ function clickHandler() {
             })
 
         .catch(errorHandler)
-};
+}
 
-btnTranslate.addEventListener("click", clickHandler)
-
+btnTranslate.addEventListener("click", clickHandler);
 
 
 
